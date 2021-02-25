28cd34756faddcad0cdc904734e932164778ffeb
hjavad <j.hasanzadeh@fartech.ir>
2021-02-25 18:55:53 +0330

    recogonize template engine

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 //port
 const port = 3000;
 
+// template engine
+app.set('view engine', 'ejs');
+
 //listen to a port
 app.listen(port);
 console.log("you are listening port " + port);
