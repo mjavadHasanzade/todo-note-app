9f68b891e679af6db0dfe976ec9760cbe0a6fc93
hjavad <j.hasanzadeh@fartech.ir>
2021-02-25 18:45:47 +0330

    config mongoose

---
+++ controllers/todoControllers.js
@@ -0,0 +1,32 @@
+
+const mongoose = require('mongoose');
+
+
+//connect to the database
+mongoose.connect('mongodb://localhost:27017/todo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("db connected");
+}).catch((err) => {
+    console.log(err);
+});
+
+
+// Create Schema
+let todoSchema = new mongoose.Schema({
+    item: String
+})
+
+// Create model
+var Todo = mongoose.model('Todo', todoSchema);
+
+
+// var itemOne = Todo({ item: 'buy something' }).save((err) => {
+//     if (err) {
+//         throw err
+//     }
+//     console.log('item saved');
+// })
+
+// var data = [{ item: 'get milk' }, { item: 'get strwberry' }, { item: 'go to library' }, { item: 'bring car' }];
